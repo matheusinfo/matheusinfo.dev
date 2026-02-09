20dca8bc31f713a1c7804d754ba198264554a626
Matheus Andrade <matheus.add12@gmail.com>
2026-02-08 21:18:20 -0300

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: 'export',
+
   compiler: {
     styledComponents: true,
   },
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
